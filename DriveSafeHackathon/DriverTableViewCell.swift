0bf5dab8b1300ce7ca0ea545a9f6edcaee3a3dac
Yasin Akinci <yasin.akinci@viseo.com>
2016-11-28 15:51:00 +0100

    Add some files

---
+++ DriveSafeHackathon/DriverTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DriverTableViewCell.swift
+//  DriveSafeHackathon
+//
+//  Created by YAK3579 on 28/11/2016.
+//  Copyright © 2016 Viseo. All rights reserved.
+//
+
+import UIKit
+
+class DriverTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
